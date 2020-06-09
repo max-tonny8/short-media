f9499e9b1fc620a48f9310888e658cca9ba8d431
tonny0831 <tonnyjansen0831@gmail.com>
2020-06-09 16:33:59 +0800

    feat: add the navBar component

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import './index.scss'
+
+type IAttrObject = {
+  name: string
+  type: number
+  url?: string
+}
+interface IProps {
+  tabList: Array<IAttrObject>
+}
+interface IState {
+  activeName: number
+}
+export default class NavBar extends React.Component<IProps, IState> {
+  public state = {
+    activeName: 1,
+  }
+  private updateActiveName(
+    event: React.MouseEvent<HTMLLIElement>,
+    activeName: number
+  ) {
+    this.setState({
+      activeName,
+    })
+  }
+  render(): React.ReactNode {
+    const lists = this.props.tabList
+    return (
+      <ul className="nav-bar-wrapper">
+        {lists.map((item, index) => {
+          return (
+            <li
+              className={`nav-item ${
+                item.type === this.state.activeName ? 'active' : ''
+              }`}
+              onClick={(event) => this.updateActiveName(event, item.type)}
+            >
+              {item.name}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+}
--- src/container/App/index.tsx
+++ src/container/App/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import NavBar from '@/components/TabBar'
+import NavBar from '@/components/NavBar'
+import TabBar from '@/components/TabBar'
 import './index.scss'
 interface IAttrObject {
   name: string
@@ -8,31 +9,44 @@ interface IAttrObject {
 }
 interface IState {
   list: Array<IAttrObject>
+  tabList: Array<IAttrObject>
 }
 export default class App extends React.Component<{}, IState> {
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      list: [
-        {
-          name: '最热',
-          type: 1,
-        },
-        {
-          name: '二次元',
-          type: 2,
-        },
-        {
-          name: '爱情',
-          type: 3,
-        },
-      ],
-    }
+  public state = {
+    list: [
+      {
+        name: '最热',
+        type: 1,
+      },
+      {
+        name: '二次元',
+        type: 2,
+      },
+      {
+        name: '科技',
+        type: 3,
+      },
+      {
+        name: '我',
+        type: 4,
+      },
+    ],
+    tabList: [
+      {
+        name: '关注',
+        type: 1,
+      },
+      {
+        name: '推荐',
+        type: 2,
+      },
+    ],
   }
-  render() {
+  render(): React.ReactElement {
     return (
       <div className="entry">
-        <NavBar list={this.state.list} />
+        <NavBar tabList={this.state.tabList} />
+        <TabBar list={this.state.list} />
       </div>
     )
   }
